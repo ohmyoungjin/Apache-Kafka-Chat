3ab44e395d6a8de706e87d95782bd9cc83b37537
ohmyougjin <sisaudwls@naver.com>
2023-01-05 18:41:51 +0900

    20220105 :: Apache-Kafka-Chat init

---
+++ src/main/java/kafka/test/kafka/service/KafkaProducer.java
@@ -0,0 +1,27 @@
+package kafka.test.kafka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    @Value(value = "${message.topic.name}")
+    private String topicName;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate kafkaTemplate) {
+        System.out.println("KafkaProducer start !!");
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        System.out.println(String.format("Produce message : %s", message));
+        this.kafkaTemplate.send(topicName, message);
+
+    }
+}
